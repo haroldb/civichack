272388328a306b19ac18db16b4e5cad7978391a0
Patrick <patrick.asare@jadu.co.uk>
2014-03-01 13:55:25 +0000

    added routes

--- index.php
+++ index.php
@@ -9,6 +9,15 @@ $app = new \Slim\Slim(array(
 $app->get('/', function () use($app) {
     $app->render('index.php');
 });
+
+$app->get('/rate-may-landlord', function () use($app) {
+    $app->render('rate-may-landlord');
+});
+
+$app->get('/view-ratings', function () use($app) {
+    $app->render('view-ratings.php');
+});
+
 $app->get('/hello/:name', function ($name) use ($app) {
     $app->view()->setData(array('name' => $name));
     $app->render('hello.php');
