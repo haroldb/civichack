ccb13378e2d47773b2b94e17b701881b6117e6ed
Harold Barker <harold.barker@jadu.co.uk>
2014-03-01 13:36:02 +0000

    Add bootstrap template

---
+++ templates/includes/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Rate My Landlord</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div class="navbar navbar-inverse navbar-fixed-top" role="navigation">
+      <div class="container">
+        <div class="navbar-header">
+          <a class="navbar-brand" href="#">Rate My Landlord</a>
+        </div>
+      </div>
+    </div>
+    <div class="container">
+      <div class="main">
--- templates/index.php
+++ templates/index.php
@@ -1,1 +1,6 @@
-Hello
+<?php
+require 'includes/header.php';
+
+echo 'Hello';
+
+require 'includes/footer.php';
