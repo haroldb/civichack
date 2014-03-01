293d559e8afb4a2d7ceab919308c5ef6b2a03aaa
Harold Barker <harold.barker@jadu.co.uk>
2014-03-01 12:47:15 +0000

    Add slim framework

--- index.php
+++ index.php
@@ -1,1 +1,17 @@
 <?php
+require 'Slim/Slim.php';
+\Slim\Slim::registerAutoloader();
+
+$app = new \Slim\Slim(array(
+        'debug' => true
+));
+
+$app->get('/', function () use($app) {
+    $app->render('index.php');
+});
+$app->get('/hello/:name', function ($name) use ($app) {
+    $app->view()->setData(array('name' => $name));
+    $app->render('hello.php');
+});
+
+$app->run();
