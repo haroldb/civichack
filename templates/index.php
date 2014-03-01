55666bd36e579e7e75db18ce3b4d9b07731e1d34
Harold Barker <git@haroldbarker.co.uk>
2014-03-01 14:31:20 +0000

    Add EULA template to index page

--- templates/index.php
+++ templates/index.php
@@ -1,11 +1,18 @@
-<?php
-require 'includes/header.php';
-?>
+<?php require 'includes/header.php'; ?>
 
 <p>
   <a href="/rate-my-landlord" class="btn btn-primary btn-lg" role="button">Rate My Landlord</a>
   <a href="/view-ratings" class="btn btn-primary btn-lg" role="button">View Ratings</a>
 </p>
 
-<?php
-require 'includes/footer.php';
+<div class="eula text-muted">
+  <h3>EULA</h3>
+  <p>
+    Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.
+    Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat.
+    At vero eos et accusam et justo duo dolores et ea rebum.
+    Ut wisi enim ad minim veniam, quis nostrud exerci tation ullamcorper suscipit lobortis nisl ut aliquip ex ea commodo consequat.
+  </p>
+</div>
+
+<?php require 'includes/footer.php'; ?>
